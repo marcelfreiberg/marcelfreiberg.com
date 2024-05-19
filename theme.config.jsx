2b22b66d1dedbd1c91a94023a49ce6c45c93811d
Marcel Freiberg <marcel.freiberg@outlook.com>
2024-05-19 11:54:14 +0200

    Add new pages and update theme configuration

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,21 +1,12 @@
-export default {
-    footer: <p>MIT 2023 © Nextra.</p>,
-    head: ({ title, meta }) => (
-        <>
-            {meta.description && (
-                <meta name="description" content={meta.description} />
-            )}
-            {meta.tag && <meta name="keywords" content={meta.tag} />}
-            {meta.author && <meta name="author" content={meta.author} />}
-        </>
-    ),
-    readMore: 'Read More →',
-    postFooter: null,
-    darkMode: false,
-    navs: [
-        {
-            url: 'https://github.com/shuding/nextra',
-            name: 'Nextra'
-        }
-    ]
+const YEAR = new Date().getFullYear()
+
+const themeConfig = {
+  footer: (
+    <small style={{ display: 'block', marginTop: '8rem' }}>
+      <time>{YEAR}</time> © Marcel Freiberg.
+      {/* <a href="/feed.xml">RSS</a> */}
+    </small>
+  )
 }
+
+export default themeConfig
